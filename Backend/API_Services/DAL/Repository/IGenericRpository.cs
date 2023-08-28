478007abcdb92eb4da6b55e24fe100821fc420dc
Deivison Jimenez <deivi.jimenez@gmail.com>
2023-08-27 21:57:34 -0400

    Se creo la rama Backend para el equipos de .Net, 1.- Se creo conexiones de paquetes nuget para entity Framework core, Sql Server, Tool 2.-se Realizo conexiones para crear cadena de conexiones 3.- se Crearon Carpetas para Models para manejo de modelos de la base de datos 4.- se Crearon carèta DAL "Data Acces Layer", para la carpeta data Context, con el archivo context con modelos y conexiones de base de datos 5.-Se crearon carpeta Repository con un archivo Interfaz IGeneryRepository con el contrato de interfaz, donde se crea las funciones CRUD 6.- a partir de este punto por cada modelo se debe crear una clase invoque este contrato y desarrolle el proceso de repository 7.posterior monte se creara el procesos BLL o capa de negocio 8. posteriormente el proceso de controlador que generara el JSON para ser leido por la vista

---
+++ Backend/API_Services/DAL/Repository/IGenericRpository.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Services.DAL.Repository
+{
+    public interface IGenericRpository<TEntityModel>where TEntityModel:class
+    {
+        Task<bool> Insertar(TEntityModel modelo);
+        Task<bool> Actualizar(TEntityModel modelo);
+        Task<bool> Eliminar(int id);
+        Task<TEntityModel> Obtnener(int id);
+        Task<IQueryable<TEntityModel>> ObtenerTodos();
+    }
+}
