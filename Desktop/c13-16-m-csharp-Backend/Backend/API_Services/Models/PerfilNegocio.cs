4eedbbc784c380cfeee8645334229560918db20b
HesherDev <nicolasmarceloarteaga@gmail.com>
2023-09-01 19:45:20 -0300

    c13-16-m-csharp-Backend

---
+++ Desktop/c13-16-m-csharp-Backend/Backend/API_Services/Models/PerfilNegocio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Services.Models;
+
+public partial class PerfilNegocio
+{
+    public int IdPerfil { get; set; }
+
+    public string? NombrePerfil { get; set; }
+
+    public virtual ICollection<DatosBasico> DatosBasicos { get; set; } = new List<DatosBasico>();
+
+    public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+
+    public virtual ICollection<Servicio> Servicios { get; set; } = new List<Servicio>();
+}
