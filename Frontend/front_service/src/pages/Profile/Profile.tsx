8187c469d3c19d62fad1599cd7fa1753f37b8d68
julianramos42 <julianete4@gmail.com>
2023-09-02 15:05:41 -0300

    estilado, faltan flechas y responsive

---
+++ Frontend/front_service/src/pages/Profile/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './profile.css'
+import cliente1 from '../../images/cliente1.webp'
+import cliente2 from '../../images/cliente2.webp'
+import cliente3 from '../../images/cliente3.webp'
+
+export default function Profile() {
+  return (
+    <main className='perfil-hero'>
+        <h1>Clientes</h1>
+        <div className='experiencia-clientes'>
+            <section className='card-cliente'>
+                <img src={cliente1} alt='perfil-cliente'></img>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
+                <h3>Nombre Cliente</h3>
+            </section>
+            <section className='card-cliente'>
+                <img src={cliente2} alt='perfil-cliente'></img>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
+                <h3>Nombre Cliente</h3>
+            </section>
+            <section className='card-cliente'>
+                <img src={cliente3} alt='perfil-cliente'></img>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
+                <h3>Nombre Cliente</h3>
+            </section>
+        </div>
+    </main>
+  )
+}
