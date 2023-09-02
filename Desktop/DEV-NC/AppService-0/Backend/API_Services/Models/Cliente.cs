2412f7adcee23d4a0a49e1bbc30cecbddc2579cb
HesherDev <nicolasmarceloarteaga@gmail.com>
2023-09-02 01:32:02 -0300

    Duplicación de la inicialización del proyecto AppService

---
+++ Desktop/DEV-NC/AppService-0/Backend/API_Services/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Services.Models;
+
+public partial class Cliente
+{
+    public int IdCliente { get; set; }
+
+    public int IdDatos { get; set; }
+
+    public decimal? Saldos { get; set; }
+
+    public virtual DatosBasico IdDatosNavigation { get; set; } = null!;
+
+    public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+}
---
+++ Desktop/DEV-NC/AppService-0/Backend/API_Services/Models/HistorialProveedore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Services.Models;
+
+public partial class HistorialProveedore
+{
+    public int IdHistorialProveedor { get; set; }
+
+    public int Renglon { get; set; }
+
+    public int IdProveedores { get; set; }
+
+    public int IdPedido { get; set; }
+
+    public decimal? Monto { get; set; }
+
+    public decimal? Saldo { get; set; }
+
+    public DateTime? Fecha { get; set; }
+}
