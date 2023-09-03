841cac056f46e8118c44534244e206fb0f30785a
julianramos42 <julianete4@gmail.com>
2023-09-03 15:07:33 -0300

    hero estilado, falta responsive

--- Frontend/front_service/src/pages/Profile/Profile.tsx
+++ Frontend/front_service/src/pages/Profile/Profile.tsx
@@ -3,6 +3,8 @@ import './profile.css'
 import cliente1 from '../../images/cliente1.webp'
 import cliente2 from '../../images/cliente2.webp'
 import cliente3 from '../../images/cliente3.webp'
+import flechaIzq from '../../images/flecha-izq.png'
+import flechaDer from '../../images/flecha-der.png'
 
 export default function Profile() {
   return (
@@ -24,6 +26,10 @@ export default function Profile() {
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
                 <h3>Nombre Cliente</h3>
             </section>
+        </div>
+        <div className='flechas'>
+            <i className="fa-solid fa-arrow-left fa-2xl"></i>
+            <i className="fa-solid fa-arrow-right fa-2xl"></i>
         </div>
     </main>
   )
