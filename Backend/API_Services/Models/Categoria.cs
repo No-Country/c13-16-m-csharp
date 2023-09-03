ce1e7770fd9080525efd70136e5209fda6c6469f
HesherDev <nicolasmarceloarteaga@gmail.com>
2023-09-03 02:19:15 -0300

    Se crearon clases e interfaces en la carpeta BLL y DAL para entidades específicas

---
+++ Backend/API_Services/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Services.Models;
+
+public partial class Categoria
+{
+    public int IdCategoria { get; set; }
+
+    public string? Categoria1 { get; set; }
+
+    public virtual ICollection<Servicio> Servicios { get; set; } = new List<Servicio>();
+}
---
+++ Backend/API_Services/Models/PedidoDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Services.Models;
+
+public partial class PedidoDetalle
+{
+    public int Renglon { get; set; }
+
+    public int IdPedidoDetalle { get; set; }
+
+    public int IdPedido { get; set; }
+
+    public int IdServicios { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public decimal? Precio { get; set; }
+
+    public decimal? TotalRenglon { get; set; }
+
+    public string? Comentario { get; set; }
+
+    public virtual Pedido IdPedidoNavigation { get; set; } = null!;
+
+    public virtual Servicio IdServiciosNavigation { get; set; } = null!;
+}
