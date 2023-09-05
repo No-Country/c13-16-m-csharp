398e4cd536dac243a2a43fa148d6d7b2f0a0e3d0
julianramos42 <julianete4@gmail.com>
2023-09-05 19:03:14 -0300

    perfil trabajador estilado, faltan detalles y responsive

--- Frontend/front_service/src/pages/Profile/Profile.tsx
+++ Frontend/front_service/src/pages/Profile/Profile.tsx
@@ -1,36 +1,12 @@
 import React from 'react'
-import './profile.css'
-import cliente1 from '../../images/cliente1.webp'
-import cliente2 from '../../images/cliente2.webp'
-import cliente3 from '../../images/cliente3.webp'
-import flechaIzq from '../../images/flecha-izq.png'
-import flechaDer from '../../images/flecha-der.png'
+import ClientProfile from '../../components/ClientProfile/ClientProfile.tsx'
+import WorkerProfile from '../../components/WorkerProfile/WorkerProfile.tsx'
 
 export default function Profile() {
   return (
-    <main className='perfil-hero'>
-        <h1>Clientes</h1>
-        <div className='experiencia-clientes'>
-            <section className='card-cliente'>
-                <img src={cliente1} alt='perfil-cliente'></img>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
-                <h3>Nombre Cliente</h3>
-            </section>
-            <section className='card-cliente'>
-                <img src={cliente2} alt='perfil-cliente'></img>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
-                <h3>Nombre Cliente</h3>
-            </section>
-            <section className='card-cliente'>
-                <img src={cliente3} alt='perfil-cliente'></img>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi error ipsam alias ad, soluta, beatae fuga ullam accusantium asperiores quis corporis tempore obcaecati, numquam officiis inventore ut dolor temporibus.</p>
-                <h3>Nombre Cliente</h3>
-            </section>
-        </div>
-        <div className='flechas'>
-            <i className="fa-solid fa-arrow-left fa-2xl"></i>
-            <i className="fa-solid fa-arrow-right fa-2xl"></i>
-        </div>
-    </main>
+    <>
+        {/* <ClientProfile/> */}
+        <WorkerProfile/>
+    </>
   )
 }
