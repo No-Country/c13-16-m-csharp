29ae668755233ff94255338295b14a0d0db9caf9
Deivison Jimenez <deivi.jimenez@gmail.com>
2023-09-06 15:20:38 -0400

    Se ingresaron 4 controladores Usuario, Datos Basicos, Perfil, Rol

--- Backend/API_Services/Controllers/PerfilNegocioController.cs
+++ Backend/API_Services/Controllers/PerfilNegocioController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API_Services.BLL.Service;
+using API_Services.Models.ViewModels;
+using API_Services.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Services.Controllers
@@ -7,5 +10,68 @@ namespace API_Services.Controllers
     [ApiController]
     public class PerfilNegocioController : ControllerBase
     {
+        private readonly IPerfilNegocioService _PerfilService;
+        
+        public PerfilNegocioController(IPerfilNegocioService PerfilService)
+        {
+            _PerfilService= PerfilService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarPerfil()
+        {
+            IQueryable<PerfilNegocio> queryPerfilSQL = await _PerfilService.ObtenerTodos();
+
+            List<VMPerfilNegocio> Lista = queryPerfilSQL
+                                .Select(c => new VMPerfilNegocio()
+                                {
+                                    IdPerfil = c.IdPerfil,
+                                    NombrePerfil = c.NombrePerfil
+
+                                }).ToList();
+            return StatusCode(StatusCodes.Status200OK, Lista);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> InsertarPerfil([FromBody] VMPerfilNegocio model)
+        {
+            PerfilNegocio NuevoModelo = new PerfilNegocio()
+            {
+                NombrePerfil = model.NombrePerfil
+            };
+
+            bool respuesta = await _PerfilService.Insertar(NuevoModelo);
+
+
+            return StatusCode(StatusCodes.Status200OK, new { valor = respuesta });
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> ActualizarPerfil([FromBody] VMPerfilNegocio model)
+        {
+            PerfilNegocio NuevoModelo = new PerfilNegocio()
+            {
+                IdPerfil = model.IdPerfil,
+                NombrePerfil = model.NombrePerfil
+            };
+
+            bool respuesta = await _PerfilService.Actualizar(NuevoModelo);
+
+
+            return StatusCode(StatusCodes.Status200OK, new { valor = respuesta });
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EliminarPerfil(int id)
+        {
+
+            bool respuesta = await _PerfilService.Eliminar(id);
+
+
+            return StatusCode(StatusCodes.Status200OK, new { valor = respuesta });
+
+        }
+
     }
 }
--- Backend/API_Services/Controllers/RolController.cs
+++ Backend/API_Services/Controllers/RolController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API_Services.BLL.Service;
+using API_Services.Models;
+using API_Services.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Services.Controllers
@@ -7,5 +10,68 @@ namespace API_Services.Controllers
     [ApiController]
     public class RolController : ControllerBase
     {
+        private readonly IRolService _rolService;
+
+        public RolController(IRolService rolService)
+        {
+            _rolService = rolService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarROL() 
+        {
+            IQueryable<Rol> queryRolSQL = await _rolService.ObtenerTodos();
+
+            List<VMRol> Lista = queryRolSQL
+                                .Select(c => new VMRol()
+                                {
+
+                                    IdRol = c.IdRol,
+                                    Roles = c.Roles
+
+                                }).ToList();
+            return StatusCode(StatusCodes.Status200OK, Lista);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> InsertarROL([FromBody] VMRol model)
+        {
+            Rol NuevoModelo = new Rol() 
+            { 
+                Roles = model.Roles
+            };
+
+            bool respuesta = await _rolService.Insertar(NuevoModelo);
+
+            
+            return StatusCode(StatusCodes.Status200OK, new { valor= respuesta} );
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> ActualizarROL([FromBody] VMRol model)
+        {
+            Rol NuevoModelo = new Rol()
+            {
+                IdRol= model.IdRol,
+                Roles = model.Roles
+            };
+
+            bool respuesta = await _rolService.Actualizar(NuevoModelo);
+
+
+            return StatusCode(StatusCodes.Status200OK, new { valor = respuesta });
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> EliminarROL(int id)
+        {
+            
+
+            bool respuesta = await _rolService.Eliminar(id);
+
+
+            return StatusCode(StatusCodes.Status200OK, new { valor = respuesta });
+
+        }
     }
 }
